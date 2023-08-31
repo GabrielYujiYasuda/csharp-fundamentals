9bc5ab573ed0cde1dff39a4227c43ee6a40699c8
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-08-30 22:23:57 -0300

    Working with Arrays

--- Array/Program.cs
+++ Array/Program.cs
@@ -49,5 +49,10 @@
         {
             Console.WriteLine(s);
         }
+
+        //for (int i = 0; i < arr.Length; i++)
+        //{
+        //    Console.WriteLine(arr[i]);
+        //}
     }
 }
