e3dd6e562806881bd2ded0e2293883256e8abbe9
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-08-31 15:30:13 -0300

    Working with Obj List

---
+++ ObjList/Aula.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjList
+{
+    public class Aula : IComparable
+    {
+        string title { get; set; }
+        public int time { get; set; }
+
+        public Aula(string title, int time)
+        {
+            this.title = title;
+            this.time = time;
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {title}\n Time: {time}\n";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Aula? temp = obj as Aula;
+
+            return title.CompareTo(temp.title);
+        }
+    }
+}
--- ObjList/Program.cs
+++ ObjList/Program.cs
@@ -1,9 +1,35 @@
-﻿internal class Program
+﻿using ObjList;
+using System.Threading.Channels;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        string aula01 = "Introdução computação";
-        string aula02 = "Estrutura de dados";
-        string aula03 = "Aprendendo dotnet";
+        Aula aula01 = new Aula("Aprendendo Rust", 999);
+        Aula aula02 = new Aula("Aprendendo Go", 12);
+        Aula aula03 = new Aula("Aprendendo Java", 65);
+        Aula aula04 = new Aula("Aprendendo Asp.net", 48);
+
+        List<Aula> list = new List<Aula>();
+
+        list.Add(aula01);
+        list.Add(aula02);
+        list.Add(aula03);
+        list.Add(aula04);
+
+        Imprimir(list);
+
+        list.Sort();
+        Imprimir(list);
+
+        list.Sort((x, y) => x.time.CompareTo(y.time));
+        Imprimir(list);
+    }
+
+    public static void Imprimir(List<Aula> list)
+    {
+        Console.Clear();
+        
+        list.ForEach(aula => Console.WriteLine(aula));
     }
 }
