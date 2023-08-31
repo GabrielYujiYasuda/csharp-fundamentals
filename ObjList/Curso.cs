204e38ead3859d5a843e3d36aa619c8c3e63e503
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-08-31 19:56:46 -0300

    Working with HashSet

---
+++ ObjList/Curso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjList
+{
+    internal class Curso
+    {
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+        public List<Aula>? Aulas { get;  set; }
+
+        public Curso(string name, string teacher, List<Aula> aulas)
+        {
+            Name = name; 
+            Teacher = teacher;
+            Aulas = aulas;
+        }
+
+        public int TotalTime()
+        {
+            return Aulas.Sum(aula => aula.Time);
+        }
+    }
+}
---
+++ Sets/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Par
+        HashSet<int> evenNumbers = new HashSet<int>();
+
+        //Ímpar
+        HashSet<int> oddNumbers = new HashSet<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            evenNumbers.Add(i * 2);
+            oddNumbers.Add((i * 2) + 1);
+        }
+
+        Console.WriteLine("Even numbers: ");
+        foreach (int i in evenNumbers)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Odd numbers: ");
+        foreach (int i in oddNumbers)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
