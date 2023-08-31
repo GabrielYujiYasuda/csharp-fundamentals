b95e78f36aa871fe87c51081abcfb8988914a25e
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-08-30 21:04:47 -0300

    Working with arrays

---
+++ Array/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string aula01 = "Introdução computação";
+        string aula02 = "Estrutura de dados";
+        string aula03 = "Aprendendo dotnet";
+
+        //string[] aulas = new string[]
+        //{
+        //    aula01, aula02, aula03
+        //};
+
+        string[] arr = new string[3];
+        arr[0] = aula01;
+        arr[1] = aula02;
+        arr[2] = aula03;
+
+        Console.WriteLine("First index: " + arr[0]);
+        Console.WriteLine("Last index: " + arr[arr.Length -1]);
+
+        Imprimir(arr);
+    }
+
+    static void Imprimir(string[] arr)
+    {
+        foreach (string s in arr)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
