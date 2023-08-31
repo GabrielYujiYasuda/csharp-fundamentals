de4af33d4d75a6c8776126eccec886b50cb8f56d
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-08-30 22:26:17 -0300

    Working with Object List

---
+++ ObjList/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string aula01 = "Introdução computação";
+        string aula02 = "Estrutura de dados";
+        string aula03 = "Aprendendo dotnet";
+    }
+}
