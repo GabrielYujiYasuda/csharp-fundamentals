a9675820a8d454df45ac291a008291bc806e071c
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-09-01 17:41:46 -0300

    Working with Strings

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Strings");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //var id = Guid.NewGuid();
+
+        var price = 12.5432;
+
+        //string text = "The price of the product is: " + price + "!Promo!";
+        //string text = string.Format("The price of produts is {0:0.000} !PROMO!", price);
+        string text = $@"The price of the product is:\n {price} !PROMO!";
+
+
+        Console.WriteLine(text);
+    }
+}
