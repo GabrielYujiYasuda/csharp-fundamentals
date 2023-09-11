c424ce8ce25983ed48d552364c4d943dbe2e8aa5
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-09-11 16:50:09 -0300

    Working with Dates

--- Datas/Program.cs
+++ Datas/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Working with datas");
+﻿using System.Data;
+using System.Diagnostics.CodeAnalysis;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        // var date = new DateTime();
+        //var date = new DateTime(2001, 03, 26);
+        var date = DateTime.Now;
+
+        Console.WriteLine("Complete: " + date);
+        Console.WriteLine("Year: " + date.Year);
+        Console.WriteLine("Month: " + date.Month);
+        Console.WriteLine("Day: " + date.Day);
+        Console.WriteLine("Hours: " + date.Hour);
+        Console.WriteLine("Minutes: " + date.Minute);
+        Console.WriteLine("Seconds: " + date.Second);
+        Console.WriteLine("Day of week: " + (int)date.DayOfWeek);
+        Console.WriteLine("Day of year: " + (int)date.DayOfYear);
+
+        Console.WriteLine("========================\n");
+
+        var formated = string.Format("{0:dd/MM/yyyy hh:mm:ss}", date);
+
+        Console.WriteLine(formated);
+
+
+    }
+}
