1740118b9a68523cb49b87d09b0cc97c681d5d41
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-09-12 23:23:08 -0300

    Working with Timezones

--- CultureInfo/Program.cs
+++ CultureInfo/Program.cs
@@ -11,7 +11,11 @@ namespace CultureInfor
             var pt = new CultureInfo("pt-BR");
             var en = new CultureInfo("en-US");
             var de = new CultureInfo("de-DE");
+
             CultureInfo currently = CultureInfo.CurrentCulture;
+
+            var utcDate = DateTime.UtcNow;
+            Console.WriteLine(utcDate);
 
             Console.WriteLine(DateTime.Now.ToString("d", de));
         }
